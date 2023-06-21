028e0efd978855155cc5d396cb52178f73da2471
fouziray <faouzirayan@gmail.com>
2023-06-21 18:01:05 +0100

    actions using code site (still vanishing)

--- API/restapp/modelEvent.py
+++ API/restapp/modelEvent.py
@@ -6,12 +6,15 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+from .models import Convo
 #class EventsManager(models.Manager):
    # def get_queryset(self):
        # return super().get_queryset().distinct('type_name')
 
 class Events(models.Model):
     sender_id = models.CharField(max_length=255)
+    #convo_id=models.ForeignKey(Convo, on_delete=models.CASCADE, related_name='messages')
     type_name = models.CharField(max_length=255)
     timestamp = models.FloatField(blank=True, null=True)
     intent_name = models.CharField(max_length=255, blank=True, null=True)
--- API/restapp/models.py
+++ API/restapp/models.py
@@ -61,14 +61,14 @@ class Convo(models.Model):
 #   user_id= models.ForeignKey(HelpProvider,on_delete=models.DO_NOTHING, related_name='user_id')
     user_id=models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name='user')
 
-class Message(models.Model):
+"""class Message(models.Model):
     convo_id= models.ForeignKey(Convo, on_delete=models.CASCADE, related_name='messages')
     created = models.DateTimeField(auto_now_add=True)
    # intent = models.ForeignKey(Intent,on_delete=models.DO_NOTHING)
     text=models.CharField(max_length=100, null=True)
     user_id= models.ForeignKey(User,on_delete=models.DO_NOTHING)
     source_is_user= models.BooleanField(default=True)   
-
+"""
 #class Intent(models.Model):
 #    intent = models.CharField(max_length=30,default="")
 #    message_id = models.ForeignKey(Message, on_delete= models.CASCADE)
