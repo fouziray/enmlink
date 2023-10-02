e817d277b6a6ae73d0e699e35d8326af22e908a9
faouzi <49080592+fouziray@users.noreply.github.com>
2023-10-02 17:35:10 +0100

    Delete actions/snmcsrpt

--- actions/enmscripting/__init__.py
+++
@@ -1,4 +0,0 @@
-from .common.element import *
-from .enmscripting import (open, close)
-from .exceptions import *
-from .security.authenticator import (UsernameAndPassword, SsoToken)
--- actions/enmscripting/private/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-enm-client-scripting private modules and classes.
-
-This is a private module and should not be imported outside of the client-scripting module.
-"""
